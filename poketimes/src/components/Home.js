02b98b0009b9f4f8700e05ae6079066e6af22278
Shameel Fazul <shameel.fazul@mail.com>
2020-07-28 18:09:25 +0530

    SRC

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
+import { connect } from 'react-redux'; //connects component to redux store.
 
 class Home extends Component {
-  state = {
-    posts: [ ]
-  }
+  // state = {
+  //   posts: [ ]
+  // }
 
-componentDidMount() {
-  axios.get('https://jsonplaceholder.typicode.com/posts') //gets data off json placeholder and returns promise - meaning its asyncronous and it promises that it will give data somepoint in time. it takes time to fetch..
-    .then(res => {                                        // 'then' method fires once the data is completely fetched.
-      console.log(res)
-      this.setState({
-        posts: res.data.slice(0,10)
-      })
-    })   
-}
+// componentDidMount() {
+//   axios.get('https://jsonplaceholder.typicode.com/posts') //gets data off json placeholder and returns promise - meaning its asyncronous and it promises that it will give data somepoint in time. it takes time to fetch..
+//     .then(res => {                                        // 'then' method fires once the data is completely fetched.
+//       console.log(res)
+//       this.setState({
+//         posts: res.data.slice(0,10)
+//       })
+//     })   
+// }
 render() {
-  const { posts } = this.state;
+  const { posts } = this.props; //changed this.state to props, because we're grabbing data using redux now.
   const postList = posts.length ? (
     posts.map(post => {
       return(
@@ -43,6 +44,15 @@ render() {
     </div>
    )
   }
+ }
+
+ const mapStateToProps = (state) => { //once connected to redux using connect(), access the store's state and map it to the props of this component.
+ return {               //return object represents the different properties we want to add to the props.
+   posts: state.posts 
+ }
 }
   
-export default Home
+export default connect(mapStateToProps)(Home)  //connect to redux using high order components. 
+                                //connect() is a function that returns the high-order component when invoked.
+                                //when returned, it then wraps (Home).
+                                //added 'mapStateToProps' as an argument to connect('') so redux knows what data to grab and then apply that data to the component's props object.
--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
+import { connect } from 'react-redux'
+import { deletePost } from '../actions/postActions'
 
 class Post extends Component {
-    state = {
-        post: null
-    }
-    componentDidMount(){
-        let id = this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-        .then(res => {
-            this.setState({
-                post: res.data         //'data' property is the data from the response. check console for more info.
-            })
-        })
+    // state = {
+    //     post: null
+    // }
+    // componentDidMount(){
+    //     let id = this.props.match.params.post_id;
+    //     axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+    //     .then(res => {
+    //         this.setState({
+    //             post: res.data         //'data' property is the data from the response. check console for more info.
+    //         })
+    //     })
+    // }
+
+    handleClick = () => {
+        this.props.deletePost(this.props.posts.id)  //calling deltePost() function with id to send a action dispatch to rootReducer.js
+        this.props.history.push('/');  //redirects user back to home page using react route props.
     }
     render() {
+        console.log(this.props)
 
-        const post = this.state.post ? (
+        const post = this.props.posts ? (
             <div className="post">
-                <h4 className="center">{ this.state.post.title }</h4>
-                <p>{ this.state.post.body }</p>
+                <h4 className="center">{ this.props.posts.title }</h4>
+                <p>{ this.props.posts.body }</p>
+                <div className="center">
+                    <button className="btn grey" onClick={ this.handleClick }>
+                        Delete Post
+                    </button>
+                </div>
             </div>
         ) : (
             <div className="center">Loading Post..</div>
@@ -33,4 +46,24 @@ class Post extends Component {
     }
 }
 
-export default Post
+const mapStateToProps = (state, ownProps) => { //ownProps is the props of this component before the additional props are added using this function.
+  let id = ownProps.match.params.post_id;
+  return {
+      posts: state.posts.find(post => post.id === id)
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch(deletePost(id)) }
+        //deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id}) } // WITHOUT ACTION CREATORS
+    }
+}
+
+//store.dispatch({type: 'ADD_TODO', todo: 'buy milk'});
+// instead of using store.dispatch, we add "dispatch" as a parameter to the function from the store.
+//call the dispatch() function inside the deletepost object.
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
